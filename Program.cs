64a867735ce8df0b575d1aeaddee2970b1e63976
Mike Mayer <mike@mikemayer.me>
2024-04-12 19:29:36 -0700

    It can do the thing

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using NAudio.Wave;
+
+var firstParameter = args.FirstOrDefault() ?? "--list-devices";
+
+if (firstParameter == "--list-devices")
+{
+    foreach (var dev in DirectSoundOut.Devices)
+    {
+        Console.WriteLine($"{dev.Guid} - {dev.Description}");
+    }
+    return;
+}
+
+Guid deviceGuid;
+var secondParameter = args.ElementAtOrDefault(1) ?? Guid.Empty.ToString();
+
+if (!Guid.TryParse(secondParameter, out deviceGuid))
+{
+    Console.WriteLine($"Invalid device uuid: {secondParameter}");
+    return;
+}
+
+using (var audioFile = new AudioFileReader(firstParameter))
+{
+    using var outputDevice = new DirectSoundOut(deviceGuid);
+    outputDevice.Init(audioFile);
+    outputDevice.Play();
+
+    if (deviceGuid == Guid.Empty)
+    {
+        Console.WriteLine($"Playing {Path.GetFileName(firstParameter)}");
+    }
+    else
+    {
+        Console.WriteLine($"Playing {Path.GetFileName(firstParameter)} on {deviceGuid}");
+    }
+
+    while (outputDevice.PlaybackState == PlaybackState.Playing)
+    {
+        Thread.Sleep(1000);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # QuickConsoleAudioPlayer
-I just needed a way to play sounds on specific hardware
+
+I just needed a way to play sounds on specific hardware devices.
+
+## Usage
+
+### No arguments
+
+```
+QuickConsoleAudioPlayer
+``
+
+This will print out an enumerated list of all sound devices found and available to DirectSound. You will want to take note of the UUID for the device you are interested in.
+
+### Just play the sound on the default device
+
+```
+QuickConsoleAudioPlayer {full path to an audio file}
+```
+
+This will just play on your device audio device. No need to think too much about it, right?
+
+
+### Play specified sound on the specified device
+
+```
+QuickConsoleAudioPlayer {full path to an audio file} {UUID for desired device}
+```
+
+Plays the specified sound on the specified device.
+
+Remember when I said you'd want to take note of the UUID for the device you wanted to play sound from? You use it here to tell QuickConsoleAudioPlayer what device to play sound from.
